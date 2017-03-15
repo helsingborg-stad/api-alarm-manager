3e26c083a570a93d9ab454fb2c4e8f725b42f21f
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-15 15:51:00 +0100

    Remove unwanted menu stuff

--- source/php/App.php
+++ source/php/App.php
@@ -6,6 +6,48 @@ class App
 {
     public function __construct()
     {
+        // Redirects
+        add_filter('login_redirect', array($this, 'loginRedirect'), 10, 3);
+        add_action('admin_init', array($this, 'dashboardRedirect'));
 
+        new Admin();
+    }
+
+    /**
+     * Redirect user after successful login.
+     *
+     * @param string $redirect_to URL to redirect to.
+     * @param string $request URL the user is coming from.
+     * @param object $user Logged user's data.
+     * @return string
+     */
+    public function loginRedirect($redirect_to, $request, $user)
+    {
+        if (!is_wp_error($user)) {
+            // Redirect to alarm list
+        }
+
+        return $redirect_to;
+    }
+
+    /**
+     * Redirect user when entering dashboard.
+     * @return void
+     */
+    public function dashboardRedirect()
+    {
+        global $pagenow;
+        if ($pagenow !== 'index.php') {
+            return;
+        }
+
+        if (isset($_GET['page']) && in_array($_GET['page'], array('acf-upgrade'))) {
+            return;
+        }
+
+        // Redirect to alarm list (remove the return here)
+        return;
+        wp_redirect(admin_url('edit.php?post_type=event'), 301);
+        exit;
     }
 }
