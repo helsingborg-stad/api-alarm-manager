71b0a43fc8a72b6907fb4a68a2ab73a6154c2d23
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-03-16 09:57:19 +0100

    Posttype fields

---
+++ source/php/AcfFields/php/station.php
@@ -0,0 +1,101 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_58ca3def60074',
+    'title' => __('Station', 'event-manager'),
+    'fields' => array(
+        0 => array(
+            'message' => '',
+            'esc_html' => 0,
+            'new_lines' => '',
+            'key' => 'field_58ca3e0e4973f',
+            'label' => __('Station ID', 'event-manager'),
+            'name' => '',
+            'type' => 'message',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+        ),
+        1 => array(
+            'default_value' => '',
+            'maxlength' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'key' => 'field_58ca3fe049740',
+            'label' => __('Street address', 'event-manager'),
+            'name' => 'street_address',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '33.3333',
+                'class' => '',
+                'id' => '',
+            ),
+        ),
+        2 => array(
+            'default_value' => '',
+            'maxlength' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'key' => 'field_58ca41bb84d75',
+            'label' => __('Postal code', 'event-manager'),
+            'name' => 'postal_code',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '33.3333',
+                'class' => '',
+                'id' => '',
+            ),
+        ),
+        3 => array(
+            'default_value' => '',
+            'maxlength' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'key' => 'field_58ca403a49741',
+            'label' => __('City', 'event-manager'),
+            'name' => 'city',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '33.3333',
+                'class' => '',
+                'id' => '',
+            ),
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'station',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}
