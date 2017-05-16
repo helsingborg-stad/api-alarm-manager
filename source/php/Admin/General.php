1f6649b6897a3871072f10a33487d28d3d1e92fe
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-05-16 10:29:52 +0200

    Remove unwanted menu items

--- source/php/Admin/General.php
+++ source/php/Admin/General.php
@@ -16,12 +16,17 @@ class General
      */
     public function removeAdminMenuItems()
     {
-        remove_menu_page('index.php');                      //Dashboard
-        remove_menu_page('edit.php');                       //Posts
-        remove_menu_page('edit.php?post_type=page');        //Pages
-        remove_menu_page('edit-comments.php');              //Comments
-        remove_menu_page('themes.php');                     //Appearance
-        remove_menu_page('tools.php');                      //Tools
+        remove_menu_page('index.php');                      // Dashboard
+        remove_menu_page('edit.php');                       // Posts
+        remove_menu_page('edit.php?post_type=page');        // Pages
+        remove_menu_page('edit-comments.php');              // Comments
+        remove_menu_page('themes.php');                     // Appearance
+        remove_menu_page('tools.php');                      // Tools
+        remove_menu_page('upload.php');                     // Media
+
+        if (!current_user_can('administrator')) {
+            remove_menu_page('options-general.php');        // Settings
+        }
     }
 
     /**
