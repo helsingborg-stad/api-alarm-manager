5ac7c67a8d8d78e6d8659caca214fab49ba22f91
GitHub Action <>
2025-08-21 07:06:27 +0000

    build: update package file versions to 2.4.1

--- api-alarm-manager.php
+++ api-alarm-manager.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Api Alarm Manager
  * Plugin URI:        https://github.com/helsingborg-stad/api-alarm-manager
  * Description:       Creates WordPress Rest API endpoint for contal alarms
- * Version: 2.4.0
+ * Version: 2.4.1
  * Author:            Kristoffer Svanmark
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
